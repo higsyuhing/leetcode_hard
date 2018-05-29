ae2253ae24f8d466fa2a532fb48eb756f6647ff7
Minjun Wu <higsyuhing@users.noreply.github.com>
2018-05-29 00:54:37 -0500

    Update 84.py

--- 84.py
+++ 84.py
@@ -1,3 +1,91 @@
+class TreeNode(object): 
+    def __init__(self, val, index, left, right):
+        self.val = val
+        self.index = index
+        self.left = left
+        self.right = right
+        pass
+        
+        
+    
+class Tree(object):
+    def __init__(self):
+        self.rootNode = None
+        pass
+    
+    def insert(self, val, index):
+        node = TreeNode(val, index, None, None)
+        if self.rootNode == None: 
+            self.rootNode = node
+            return
+        else: 
+            temp = self.rootNode
+            parent = temp
+            while temp.val < node.val: 
+                if temp.index > node.index: 
+                    parent = temp
+                    temp = temp.left
+                    pass
+                else: 
+                    parent = temp
+                    temp = temp.right
+                    pass
+                if temp == None: 
+                    break
+                    pass
+                pass
+            # 2 cases: temp.val > node.val; temp == None
+            if temp == None: 
+                if parent.index > node.index: 
+                    parent.left = node
+                    return
+                else: 
+                    parent.right = node
+                    return
+                pass
+            else: 
+                #............. re-construct????
+    
+
+class Solution(object):
+    def getMax(self, sortTree, i, j): 
+        # sortTree: tree of (value, heights index), left, right
+        # get Max from [i, j)
+        if i >= j: 
+            return 0
+        Minheight = sortheights[0][0]
+        Max0 = (j-i)*Minheight
+        k = sortheights[0][1]
+        subsort1 = []
+        subsort2 = []
+        for index in range(len(sortheights)-1): 
+            curr = index + 1
+            if sortheights[curr][1] < k: 
+                subsort1.append(sortheights[curr])
+                pass
+            else: 
+                subsort2.append(sortheights[curr])
+                pass
+            pass
+        Max1 = self.getMax(subsort1, i, k)
+        Max2 = self.getMax(subsort2, k+1, j)
+        return max(Max0, Max1, Max2)
+    
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        sortheights = []
+        for index in range(len(heights)): 
+            sortheights.append([heights[index], index, 0, 0])
+            pass
+        sortheights.sort()
+        return self.getMax(sortheights, 0, len(heights))
+
+    
+
+'''
 class Solution(object):
     def getMax(self, sortheights, i, j): 
         # sortheights: list of (value, heights index)
@@ -33,9 +121,9 @@ class Solution(object):
             pass
         sortheights.sort()
         return self.getMax(sortheights, 0, len(heights))
-        
-
 '''
+
+'''     
 class Solution(object):
     def Base(self, heights, i, j):
         # base for range [i, j)
