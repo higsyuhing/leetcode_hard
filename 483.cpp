c964c197807c78d9fb6ac8e6d635187c83e146d7
higsyuhing <higsyuhing@users.noreply.github.com>
2022-08-24 18:10:10 -0500

    Create 483.cpp

---
+++ 483.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string smallestGoodBase(string n) {
+        // n = sum of base^i, i from 0 to k
+        // int sum = 0; for (i = 0 to k) sum += base^i; n == sum. 
+        // but 2 axes: base and k value. 
+        // we can have either small base and larger k, 
+        //      or larger base and smaller k. 
+        
+    }
+};
+
+/*
+
+https://leetcode.com/problems/smallest-good-base/discuss/96587/Python-solution-with-detailed-mathematical-explanation-and-derivation
+
+OK.. I dont know math.. 
+
+*/
